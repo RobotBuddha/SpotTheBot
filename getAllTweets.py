155872ea3de287f8e98f36455c3b42436636d36d
Angus Shaw <shawa950@gmail.com>
2018-03-10 17:10:27 +0000

    Add files via upload

---
+++ getAllTweets.py
@@ -0,0 +1,24 @@
+import tweepy
+import csv
+
+ckey = '07Iv3RJnRPPToDS6a564cQ93h'
+csecret = 'J19Zx8NsJo03R5WfiqVTu7lfmPySLrfd2IIGtzPvdXCEAUG9YB'
+akey = '748975287019405312-EfE9siMvpp4LS3McSj8EOmMxB3eLBuP'
+asecret = 'gzgZfuSNlE5e1F3VFsFyLfdWAVSom7gSuWavX7PnJ4fbU'
+
+def getAllTweets(screen_name):
+	auth = tweepy.OAuthHandler(ckey, csecret)
+	auth.set_access_token(akey, asecret)
+	api = tweepy.API(auth)
+	alltweets = []
+	new_tweets = api.user_timeline(screen_name = screen_name,count=200)
+	alltweets.extend(new_tweets)
+	oldest = alltweets[-1].id - 1
+	while len(new_tweets) > 0:
+		print "getting tweets before %s" % (oldest)
+		new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
+		alltweets.extend(new_tweets)
+		oldest = alltweets[-1].id - 1
+		print "...%s tweets downloaded so far" % (len(alltweets))
+	outtweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in alltweets]
+	return outtweets
