a8edb98105dec69ab117ceaf84a53c6c772fbd72
Angus Shaw <shawa950@gmail.com>
2018-03-10 17:12:09 +0000

    Update getAllTweets.py

--- getAllTweets.py
+++ getAllTweets.py
@@ -15,10 +15,8 @@ def getAllTweets(screen_name):
 	alltweets.extend(new_tweets)
 	oldest = alltweets[-1].id - 1
 	while len(new_tweets) > 0:
-		print "getting tweets before %s" % (oldest)
 		new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
 		alltweets.extend(new_tweets)
 		oldest = alltweets[-1].id - 1
-		print "...%s tweets downloaded so far" % (len(alltweets))
 	outtweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in alltweets]
 	return outtweets
