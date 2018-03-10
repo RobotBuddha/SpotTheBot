ffe16c0980b7db5cd3ad6e9a3382cd3631e1b49b
Angus Shaw <shawa950@gmail.com>
2018-03-10 18:47:25 +0000

    Update getAllTweets.py

--- getAllTweets.py
+++ getAllTweets.py
@@ -7,16 +7,15 @@ akey = '748975287019405312-EfE9siMvpp4LS3McSj8EOmMxB3eLBuP'
 asecret = 'gzgZfuSNlE5e1F3VFsFyLfdWAVSom7gSuWavX7PnJ4fbU'
 
 def getAllTweets(screen_name):
-	auth = tweepy.OAuthHandler(ckey, csecret)
-	auth.set_access_token(akey, asecret)
-	api = tweepy.API(auth)
+	authorise = tweepy.OAuthHandler(ckey, csecret)
+	authorise.set_access_token(akey, asecret)
+	api = tweepy.API(authorise)
 	alltweets = []
-	new_tweets = api.user_timeline(screen_name = screen_name,count=200)
-	alltweets.extend(new_tweets)
+	newTweets = api.user_timeline(screen_name = screen_name,count=200)
+	alltweets.extend(newTweets)
 	oldest = alltweets[-1].id - 1
-	while len(new_tweets) > 0:
-		new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
-		alltweets.extend(new_tweets)
+	while len(newTweets) > 0:
+		newTweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
+		alltweets.extend(newTweets)
 		oldest = alltweets[-1].id - 1
-	outtweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in alltweets]
-	return outtweets
+	return alltweets
