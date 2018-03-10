d49858af8832de85465a3c7480c7b4beb0c1e051
Angus Shaw <shawa950@gmail.com>
2018-03-10 18:35:32 +0000

    Update and rename feature_percent_of_tweets_that_are_links.py to feature_percent_links.py

--- feature_percent_links.py
+++ feature_percent_links.py
@@ -5,11 +5,12 @@ import re
 def linkRatio(tweets):
     filteredTweets = 0
     for tweet in tweets:
-        if (re.search('((www\.[^\s]+)|(https?://[^\s]+))', tweet[2]) is not None):
+        if (re.search('((www\.[^\s]+)|(https?://[^\s]+))', tweet.text) is not None):
             filteredTweets = filteredTweets + 1
     return (filteredTweets*1.0)/len(tweets)
 
 # Main runs the method on Donald Trump's Twitter
 if __name__ == '__main__':
+    #ratio = linkRatio(getAllTweets("J_tsar"))
     ratio = linkRatio(getAllTweets("@realDonaldTrump"))
     print "ratio for '@realDonaldTrump': " + str(ratio)
