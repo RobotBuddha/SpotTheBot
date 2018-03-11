f3cc16a7b8d823ce6a5622741caf06182993fed7
Angus Shaw <shawa950@gmail.com>
2018-03-11 12:33:39 +0000

    Tests Trump, a clickbaiter and a false name.

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
-import tell_if_bot
+from tell_if_bot import *
 
-print tell_if_bot.testAccount("Henrylaborbe")
+#print tell_if_bot.testAccount("Henrylaborbe")
+
+print ""
+print "Test on @realDonaldTrump:"
+print (testAccount("realDonaldTrump"))
+print ""
+print "Test on @SandraFair66:"
+print (testAccount("SandraFair66"))
+print ""
+print "Test on @q923c4q2qn23q43:"
+print (testAccount("q923c4q2qn23q43"))
+print ""
